c9736a6c664f5e47982d080c19c04890b4002f0d
Tamicktom <rickpokemon38@gmail.com>
2024-08-31 09:22:29 -0300

    install custom font

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,13 @@
 export function App(): JSX.Element {
-  return <h1>Hello World</h1>
+  return (
+    <h1
+      style={{
+        fontFamily: 'Inter',
+        fontSize: '24px',
+        fontWeight: '500'
+      }}
+    >
+      Hello World
+    </h1>
+  )
 }
