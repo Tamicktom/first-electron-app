1c22ac9e806f58a596da49f4d6b7a748d68614c5
Tamicktom <rickpokemon38@gmail.com>
2024-08-31 09:43:54 -0300

    install tailwind

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,13 +1,3 @@
 export function App(): JSX.Element {
-  return (
-    <h1
-      style={{
-        fontFamily: 'Inter',
-        fontSize: '24px',
-        fontWeight: '500'
-      }}
-    >
-      Hello World
-    </h1>
-  )
+  return <h1 className="font-sans text-4xl font-bold text-red-600">Hello World</h1>
 }
